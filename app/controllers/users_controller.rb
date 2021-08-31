aade48e91e99372d49cea9de0f67ea7957f1266a
Alexander <a.makarov@molinos.ru>
2021-08-31 10:33:36 +0700

    search skeleton

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,30 @@ class UsersController < ApplicationController
     @current_user = current_user
     @tracked_stocks = current_user.stocks
   end
+
+  def index
+    @users = User.all
+    @friends = current_user.friends
+  end
+
+  def search
+    if params[:search_request].present?
+      @user = User.first
+      if @user
+        respond_to do |format|
+          format.js { render partial: 'users/users_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = 'Please enter a valid user search request'
+          format.js { render partial: 'users/users_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Request is empty'
+        format.js { render partial: 'users/users_result' }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: %i[create destroy]
+  resources :users, only: %i[index]
   get 'stocks/search'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'homepage#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
+  get 'search_user', to: 'users#search'
 end
